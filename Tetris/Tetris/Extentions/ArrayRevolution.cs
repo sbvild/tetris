339f72cfbb54aef7926c1c00590c737f7ae886c3
s bvild <hogedesu@gmail.com>
2013-06-01 21:39:38 +0900

    first

---
+++ Tetris/Tetris/Extentions/ArrayRevolution.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    //もらってきた
+    //http://gushwell.ifdef.jp/etude/ArrayRevolution.html
+      static class RotateArray {
+        // これが基準
+          public static bool[,] Rotate90(this bool[,] array)
+          {
+              bool[,] newarray = new bool[array.GetLength(1), array.GetLength(0)];
+              int i = 0;
+
+              for (int x = 0; x < array.GetLength(1); ++x)
+              {
+                  for (int y = array.GetLength(0) - 1; y >= 0; --y)
+                  {
+                      newarray[i / array.GetLength(0), i % array.GetLength(0)] = array[y, x];
+                      i++;
+                  }
+
+              }
+              return newarray;
+          }
+    }
+
+
+}
